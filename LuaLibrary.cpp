b2bf7e0e6454d07541f33e1dac3c8393b5356a5f
Kelson Hootman <kelson@lionside.com>
2012-11-22 00:42:25 -0800

    Add support for local environments to Core.

--- LuaLibrary.cpp
+++ LuaLibrary.cpp
@@ -1,5 +1,5 @@
 #include "LuaLibrary.h"
-
+#include <functional>
 
 KEngineCore::LuaLibrary::LuaLibrary(void)
 {
@@ -23,4 +23,57 @@ void KEngineCore::LuaLibrary::PreloadLibraryIntoTable(lua_State * luaState, char
 	lua_pushcclosure(luaState, libraryFunction, 1);
 	lua_setfield(luaState, -2, name);
 	lua_pop(luaState, 1);
+}
+
+
+static int localRequire(lua_State * luaState) {
+	luaL_checktype(luaState, 1, LUA_TSTRING);
+	lua_pushvalue(luaState, 1);
+	lua_gettable(luaState, lua_upvalueindex(1)); //Upvalue 1 is the local "loaded" table
+	if (lua_type(luaState, -1) != LUA_TNIL) {  //If there's something there, return it
+		return 1;
+	}
+	lua_pop(luaState, 1);//otherwise pop
+	lua_pushvalue(luaState, 1);
+	lua_gettable(luaState, lua_upvalueindex(2)); //Upvalue 2 is the local "preload" table
+	if (lua_type(luaState, -1) == LUA_TFUNCTION) {  //If there's something there, call it and return what it returns
+		lua_pushvalue(luaState, 1); //Push the name of the module again
+		lua_call(luaState, 1, 1); //Call the module loader with one return value
+		lua_pushvalue(luaState, 1); //Push the name of the module again
+		lua_pushvalue(luaState, -2); //Push the loader's return value
+		lua_settable(luaState, lua_upvalueindex(1)); //Set the return value of the loader into the local "loaded" table
+		return 1;
+	}
+	lua_pop(luaState, 1); //otherwise pop
+	lua_pushvalue(luaState, lua_upvalueindex(3)); //Upvalue 3 is the original "require" function
+	lua_pushvalue(luaState, 1); //Push the module name
+	lua_call(luaState, 1, 1);
+	return 1;
+}
+
+void KEngineCore::LuaLibrary::CreateLocalEnvironment(lua_State * scriptState, std::function<void (lua_State *)> registerLocalLibraries) {
+	lua_getupvalue(scriptState, -1, 1); //Get the current environment (NASTY)
+	lua_newtable(scriptState); //New table to be the custom environment
+	lua_newtable(scriptState); //New table to be metatable of custom environment
+	lua_pushvalue(scriptState, -3); //Get the old environment
+	lua_setfield(scriptState, -2, "__index"); //Set up the replacement's metatable to forward to the old table
+	lua_setmetatable(scriptState, -2); //Set the replacement's metatable
+	lua_replace(scriptState, -2); //replace the old table with the replacement
+	
+	//Create a custom require function that checks local libraries first
+	lua_newtable(scriptState); //new table to serve as loaded
+	lua_newtable(scriptState); //new table to serve as preload
+	
+	//Register local library
+	registerLocalLibraries(scriptState);
+
+	lua_getglobal(scriptState, "require"); //original require for fallback
+	lua_pushcclosure(scriptState, localRequire, 3); //push the new require function
+	lua_pushstring(scriptState, "require"); //push the name require
+	lua_pushvalue(scriptState, -2); //re-push the require function
+	lua_remove(scriptState, -3); //remove the original copy of the new require function
+	lua_rawset(scriptState, -3); //add it to the environment
+
+	//Set the new environment as the first upvalue of the script
+	lua_setupvalue(scriptState, -2, 1);
 }
