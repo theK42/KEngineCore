07b886be7a48c95f60fe9bed9bbcbdef87d9dcbc
Kelson Hootman <theK42@gmail.com>
2017-12-03 09:47:28 -0800

    Add TextFile support

---
+++ TextFile.cpp
@@ -0,0 +1,26 @@
+#include "TextFile.h"
+#include <iostream>
+#include <fstream>
+
+
+void KEngineCore::TextFile::LoadFromFile(const std::string& filename, const std::string& extension)
+{
+	std::ifstream inFile;
+	inFile.open(filename + extension, std::ifstream::in | std::ifstream::binary);
+	if (!inFile) {
+		std::cerr << "Unable to open file datafile.txt";
+		exit(1);   // call system to stop
+	}
+
+	inFile.seekg(0, std::ios::end);
+	mFileContents.resize(inFile.tellg()); //Gets file size.  Tellg
+	inFile.seekg(0, std::ios::beg);
+	inFile.read(&mFileContents[0], mFileContents.size());
+	inFile.close();
+}
+
+const std::string& KEngineCore::TextFile::GetContents() const 
+{
+	return mFileContents;
+	
+}
